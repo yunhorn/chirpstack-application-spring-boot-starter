ba9349535d8eb0e6217c0514aa43df3bfa1c25a7
linjiming <jiming.lin@yunhorn.com>
2021-12-22 18:44:55 +0800

    提交配置文件

---
+++ src/main/java/com/yunhorn/core/chirpstack/ChirpstackAppSyncerApplication.java
@@ -0,0 +1,13 @@
+package com.yunhorn.core.chirpstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirpstackAppSyncerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChirpstackAppSyncerApplication.class, args);
+	}
+
+}
